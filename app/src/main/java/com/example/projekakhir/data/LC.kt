265f08d8cdc89abe5abecec312b3314a15fab109
panjialbir <115150326+panjialbir@users.noreply.github.com>
2024-01-12 02:08:50 +0700

    menambahkan LC

---
+++ app/src/main/java/com/example/projekakhir/data/LC.kt
@@ -0,0 +1,11 @@
+package com.example.projekakhir.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "table_lc")
+data class LC(
+    @PrimaryKey(autoGenerate = true)
+    val lcid: Long = 0,
+    val lcname: String
+)
