4f76da8e23e8f1261293a6788d58d4b5b907b0a5
panjialbir <115150326+panjialbir@users.noreply.github.com>
2024-01-12 02:29:41 +0700

    menambahkan containerapp

---
+++ app/src/main/java/com/example/projekakhir/repository/ContainerApp.kt
@@ -0,0 +1,14 @@
+package com.example.projekakhir.repository
+
+import android.content.Context
+import com.example.projekakhir.data.DatabaseOrder
+
+interface ContainerApp{
+    val repositoryOrder : RepositoryOrder
+}
+
+class ContainerDataApp(private val context: Context): ContainerApp{
+    override val repositoryOrder: RepositoryOrder by lazy {
+        OfflineRepositoryOrder(DatabaseOrder.getDatabase(context).orderRoomDao())
+    }
+}
