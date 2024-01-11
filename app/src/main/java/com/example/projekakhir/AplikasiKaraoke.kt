8df144be27533b80228f5f19b561e079371d1bbb
panjialbir <115150326+panjialbir@users.noreply.github.com>
2024-01-12 02:52:37 +0700

    menambahkan file class aplikasi

---
+++ app/src/main/java/com/example/projekakhir/AplikasiKaraoke.kt
@@ -0,0 +1,17 @@
+package com.example.projekakhir
+
+import android.app.Application
+import com.example.projekakhir.repository.ContainerApp
+import com.example.projekakhir.repository.ContainerDataApp
+
+class AplikasiKaraoke: Application() {
+
+
+        lateinit var container: ContainerApp
+
+        override fun onCreate() {
+            super.onCreate()
+            container = ContainerDataApp(this)
+        }
+    }
+}
