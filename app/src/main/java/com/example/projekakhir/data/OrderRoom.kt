ad004d559f268de33c198be9a31343710a72d62a
panjialbir <115150326+panjialbir@users.noreply.github.com>
2024-01-12 02:07:41 +0700

    menambahkan orderroomdao

--- app/src/main/java/com/example/projekakhir/data/OrderRoom.kt
+++ app/src/main/java/com/example/projekakhir/data/OrderRoom.kt
@@ -1,3 +1,16 @@
 package com.example.projekakhir.data
 
-data class OrderRoom()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "penyewaan_karaoke")
+data class OrderRoom(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val namaPelanggan: String,
+    val tanggalSewa: String,
+    val durasiSewaJam: Int,
+    val hargaTotal: Double
+)
+
+
---
+++ app/src/main/java/com/example/projekakhir/data/OrderRoomDao.kt
@@ -0,0 +1,32 @@
+package com.example.projekakhir.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface OrderRoomDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun tambahPenyewaan(orderRoom: OrderRoom)
+
+
+    @Update
+    suspend fun update(orderRoom: OrderRoom)
+
+    @Delete
+    suspend fun delete(orderRoom: OrderRoom)
+
+    @Query("SELECT * from penyewaan_karaoke WHERE id = :id")
+    fun getOrder(id: Int): Flow<OrderRoom>
+
+    @Query("SELECT * from penyewaan_karaoke ORDER BY namaPelanggan ASC")
+    fun getAllOrder() : Flow<List<OrderRoom>>
+
+
+
+}
+
